95e4cba3b1fe7576534582bc2b0518e89c12424c
Chibuzor Alumba <coolchibs@gmail.com>
2018-12-26 16:39:28 -0600

    changed the app to inject the baseurl rather than hardcode it in the environment var

--- CartwheelDemo.Web/ClientApp/src/app/_services/user.service.ts
+++ CartwheelDemo.Web/ClientApp/src/app/_services/user.service.ts
@@ -1,63 +1,67 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RequestOptions } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { User } from '../_models';
-import { environment } from '../../environments/environment';
 
 @Injectable()
 export class UserService {
+  apiUrl = '';
   private options = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json;odata=verbose' })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.apiUrl = this.baseUrl + 'api/users';
+    // console.log('api url ', this.apiUrl);
+
+  }
 
   getAll() {
-    let url = `${environment.apiUrl}/users`;
-    return this.http.get<User[]>(url);
+    // const url = `${this.apiUrl}/users`;
+    return this.http.get<User[]>(this.apiUrl);
   }
 
   getById(id: number) {
-      return this.http.get(`${environment.apiUrl}/users/${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 
   getByEmailAndPassword(email: string, password: string) {
-    let user = new User();
+    const user = new User();
     user.email = email;
     user.password = password;
-    return this.http.post(`${environment.apiUrl}/users/Login`, user);
+    return this.http.post(`${this.apiUrl}/Login`, user);
   }
 
   register(user: User) {
-    let newUser = new User();
+    const newUser = new User();
     newUser.email = user.email;
     newUser.firstName = user.firstName;
     newUser.companyName = user.companyName;
     newUser.lastName = user.lastName;
     newUser.password = user.password;
 
-    
-    //let options = new RequestOptions({ headers: headers });
 
-    return this.http.post(`${environment.apiUrl}/users/register`, newUser, this.options);
+    // let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(`${this.apiUrl}/register`, newUser, this.options);
   }
 
   login(email: string, password: string) {
-    
-    return this.http.post<any>(`${environment.apiUrl}/users/login`, { email, password });
+
+    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
   }
 
   isUserAuthenticated(token: string) {
-    
-    return this.http.post(`${environment.apiUrl}/users/isuserauthenticated`, JSON.stringify(token), this.options);
+
+    return this.http.post(`${this.apiUrl}/isuserauthenticated`, JSON.stringify(token), this.options);
   }
 
   update(user: User) {
-      return this.http.put(`${environment.apiUrl}/users/${user.id}`, user);
+    return this.http.put(`${this.apiUrl}/${user.id}`, user);
   }
 
   delete(id: number) {
-      return this.http.delete(`${environment.apiUrl}/users/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
--- CartwheelDemo.Web/ClientApp/src/environments/environment.ts
+++ CartwheelDemo.Web/ClientApp/src/environments/environment.ts
@@ -5,5 +5,4 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44323/api'
 };
